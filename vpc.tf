a0135ac06b80b1526b77fc81332e755807918d0c
passportking <demetri413stocks@gmail.com>
2025-03-21 00:22:59 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 //1. creat the vpc
 resource "aws_vpc" "custom_vpc" {
-  cidr_block           = "10.230.0./16"
+  cidr_block           = "10.230.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
